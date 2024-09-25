f22e17cbfe3fbef119b8f1eb1206fca5e6a15b25
bryreed <69174354+bryreed@users.noreply.github.com>
2024-09-25 16:57:45 -0500

    Update outputs.tf

--- module/ids/outputs.tf
+++ module/ids/outputs.tf
@@ -6,11 +6,3 @@ output "NumberOfIDSAppliancesDeployed" {
   value = var.ids_appliance_number
 }
 
-output "IDSSecurityGroupID" {
-  value = aws_cloudformation_stack.idsappliance.outputs["SecurityGroupId"]
-}
-
-output "IDSDeployedInSubnetIDs" {
-  value = "${var.ids_subnet_id}/${var.ids_subnet_type}"
-}
-
