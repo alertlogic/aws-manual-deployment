0167c75fc5bec7ca8eb38e323aec1a6c61e915f4
bryreed <69174354+bryreed@users.noreply.github.com>
2024-09-25 16:58:18 -0500

    Update outputs.tf

--- module/ci_scan/outputs.tf
+++ module/ci_scan/outputs.tf
@@ -2,11 +2,3 @@ output "ProtectedVPC" {
   value = var.vpc_id
 }
 
-output "CISecurityGroupID" {
-  value = aws_cloudformation_stack.scanappliance.outputs["SecurityGroupId"]
-}
-
-output "ScannerDeployedInSubnetID" {
-  value = "${var.ci_subnet_id}/${var.ci_subnet_type}"
-}
-
